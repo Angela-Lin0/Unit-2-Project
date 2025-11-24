c1cb73cee7aca5cd7ae78bb06f2245f77c858287
Angela-Lin0 <angelal172@nycstudents.net>
2025-11-24 14:49:53 -0500

    Add files via upload
    11-24 Commit

--- Main.java
+++ Main.java
@@ -26,42 +26,58 @@ public class Main {
 
         if (dice1 == dice2) {
             System.out.println("You win! The two dice are the same.");
+            tries += 1;
+            streak += 1;
+            System.out.println("tries; " + tries);
+            System.out.println("streak: " + streak);
         } else {
+            streak = 0;
+            tries += 1;
             System.out.println("You lost. You can try again or quit (a=try again, q=quit)");
-            userInput = scan.nextLine();
-            if (userInput.equals("q")) {
-                System.out.println("Game over");
-            } else {
-                while (!(dice1 == dice2)) {
-                    System.out.println("Press r to roll again!");
-
-                    userInput = scan.nextLine();
-
-                    if (userInput.equals("r")) {
-                        dice1 = Dice.diceRoll();
-                        System.out.println("Dice 1 is " + dice1);
-                        dice2 = Dice.diceRoll();
-                        System.out.println("Dice 2 is " + dice2);
-                    }
-                    if (dice1 == dice2) {
-                        System.out.println("You win!");
-                    } else {
-                        System.out.println("You can try again or quit (a=try again, q=quit)");
-
-
-                    }
-                }
-            }
+        }
+        userInput = scan.nextLine();
+        if (userInput.equals("q")) {
+            System.out.println("Game over");
+            System.out.println("tries; " + tries);
+            System.out.println("streak: " + streak);
+        } else {
+            System.out.println("Press r to roll again!");
         }
 
+        while (!(dice1 == dice2)) {
+            userInput = scan.nextLine();
 
-
-
-
-            if (dice1 == dice2) {
-                streak += 1;
+            if (userInput.equals("r")) {
+                dice1 = Dice.diceRoll();
+                System.out.println("Dice 1 is " + dice1);
+                dice2 = Dice.diceRoll();
+                System.out.println("Dice 2 is " + dice2);
             }
+            if (dice1 == dice2) {
+                System.out.println("You win!");
+                tries += 1;
+                streak += 1;
+                System.out.println("tries: " + tries);
+                System.out.println("streak: " + streak);
+            } else {
+                userInput.equals("");
+                System.out.println("You can try again or quit (a=try again, q=quit)");
+                userInput = scan.nextLine();
 
+                if (userInput.equals("q")) {
+                    System.out.println("Game over");
+                    System.out.println("tries: " + tries);
+                    System.out.println("streak: " + streak);
+                } else {
+                    System.out.println("Press r to roll again!");
+                    tries += 1;
+                }
+
+            }
         }
     }
+}
+
+
+
 
